01425f316a159aa5cf62a6625f304b0267e0626e
Kati McConaghy <kmcc079@gmail.com>
2023-02-24 17:19:42 -0500

    first commit

---
+++ hw1.js
@@ -0,0 +1,38 @@
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
+
+function findWords() {
+    for(let i of dog_names){
+        if(dog_string.match(i)){
+            console.log(`Matched ${i} \n`)
+        } else {
+            console.log('No Matches')
+        }
+    }
+    return 'complete'
+}
+console.log(findWords())
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index" */
+
+// GIVEN ARRAY:
+let arr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+function replaceEvens(arr){
+    for(let i = 0; i < arr.length; i += 2){
+        arr.splice(i, 1, 'even index')
+    }
+    return arr
+}
+console.log(replaceEvens(arr))
+
+//Expected output
+//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
